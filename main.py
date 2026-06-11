05ba759d4e74f8e7c0845b016ff5fa15007c8e5a
leekahhan-del <lee_kah_han@s2026.ssts.edu.sg>
2026-06-11 20:02:35 +0800

    Adding a way to get out of the Crafting state(Esc)
    added recipe dictionary(going to use for future things)

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ state = 'game'
 
 collectables = {"apple": pygame.Rect(0, 0, 50, 50), "gun": pygame.Rect(750, 0, 50, 50)}
 inventory_visibility = False
+craft_rect = pygame.Rect(sw/2-50, sh/2-50, 100, 100)
+
+recipes = {
+    'explosive apple': ['apple', 'gun']
+}
 
 class Player():
     def __init__(self, x, y):
@@ -35,6 +40,81 @@ class Player():
             self.xvel -= 3
             ix = True
         if key[pygame.K_RIGHT] or key[pygame.K_d]:
+            self.xvel += 3
+            ix = True
+
+        if not ix:
+            self.xvel *= 0.5
+        if not iy:
+            self.yvel *= 0.5
+        
+        self.xvel = max(-10, min(10, self.xvel))
+        self.yvel = max(-10, min(10, self.yvel))
+
+        self.x += self.xvel
+        self.y += self.yvel
+    
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 255, 255), (self.x, self.y, 50, 50))
+    
+    def collide(self, collectables, craft_rect, key):
+        global state
+        collected = []
+        rect = pygame.Rect(self.x, self.y, 50, 50)
+        for name, item_rect in collectables.items():
+            if rect.colliderect(item_rect):
+                if len(self.inventory) < 5:
+                    collected.append(name)
+                    self.inventory.append(name)
+                    print(name)
+        
+        if rect.colliderect(craft_rect):
+            if key[pygame.K_c]:
+                state = 'craft'
+        
+        return(collected)
+        
+p1 = Player(sw/2 - 25, sh/2 - 25)
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                inventory_visibility = not inventory_visibility
+    s.fill((0, 0, 0))
+
+    keys = pygame.key.get_pressed()
+
+    if state == 'game':
+        for name, rect in collectables.items():
+            pygame.draw.rect(s, (255, 255, 0), rect)
+        
+        pygame.draw.rect(s, (150, 75, 0), craft_rect)
+            
+
+        p1.move(keys)
+        for item in p1.collide(collectables, craft_rect, keys):
+            del collectables[str(item)]
+        p1.draw(s)
+
+        # ui
+        if inventory_visibility:
+            for i, item in enumerate(p1.inventory):
+                text = font.render(item, True, (255, 255, 255))
+                s.blit(text, (10, 10 + i * 30))
+    elif state == 'craft':
+        if keys[pygame.K_ESCAPE]:
+            state = 'game'
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()        if key[pygame.K_RIGHT] or key[pygame.K_d]:
             self.xvel += 3
             ix = True
 
