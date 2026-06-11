2330fa0a564c6e12e8d97a02e01d60a2456f79ed
leekahhan-del <lee_kah_han@s2026.ssts.edu.sg>
2026-06-11 19:07:50 +0800

    Implement Player class for character movement
    Added Player class with movement functionality.

--- main.py
+++ main.py
@@ -4,6 +4,39 @@ pygame.init()
 
 s = pygame.display.set_mode((800, 600))
 
+class Player():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.xvel = 0
+        self.yvel = 0
+        self.inventory = []
+    
+    def move(self, key):
+        ix = False
+        iy = False
+        if key[pygame.K_UP] or key[pygame.K_w]:
+            self.yvel -= 5
+            iy = True
+        if key[pygame.K_DOWN] or key[pygame.K_s]:
+            self.yvel += 5
+            iy = True
+        if key[pygame.K_LEFT] or key[pygame.K_a]:
+            self.xvel -= 5
+            ix = True
+        if key[pygame.K_RIGHT] or key[pygame.K_d]:
+            self.xvel += 5
+            ix = True
+
+        if not ix:
+            xvel *= 0.5
+        if not iy:
+            yvel *= 0.5
+        
+        self.xvel = max(-10, min(10, self.xvel))
+        self.yvel = max(-10, min(10, self.yvel))
+        
+
 running = True
 
 while running:
